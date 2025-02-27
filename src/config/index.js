181871759d67b3a5e7bfce4025c6b0c524367873
kingaimaster94 <kingaimaster94@gmail.com>
2025-02-27 19:20:34 +0000

    add omax testnet

--- src/config/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    API_URL: `${process.env.NEXT_PUBLIC_APP_API_URL}/api/kcoin/public`,
-    EXPLORE_URL: "https://testnet.bscscan.com",
-  };
--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,10 +1,23 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import { defineChain } from 'viem';
 import {
   mainnet,
   bsc,
   omax,
   bscTestnet
 } from 'wagmi/chains';
+
+export const omaxtestnet = defineChain({
+  id: 332,
+  name: 'Omax Testnet',
+  nativeCurrency: { name: 'OmaxT', symbol: 'OMAXT', decimals: 18 },
+  rpcUrls: {
+    default: { http: ['https://testapi.omaxray.com'] },
+  },
+  blockExplorers: {
+    default: { name: 'Omaxscan', url: 'https://testnet.omaxscan.com' },
+  },
+});
 
 export const config = getDefaultConfig({
   appName: 'RainbowKit demo',
@@ -13,7 +26,7 @@ export const config = getDefaultConfig({
     mainnet,
     bsc,
     omax,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [bscTestnet] : []),
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [bscTestnet, omaxtestnet] : []),
   ],
   ssr: true,
 });
